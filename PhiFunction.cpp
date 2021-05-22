3f8e926c9da6a5bebb2206dd0c1bf63278524df5
ChitreshApte <60211927+ChitreshApte@users.noreply.github.com>
2021-05-22 22:03:01 +0530

    euler totient function

---
+++ PhiFunction.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 1e6;
+int phi[N]; //phi[i] = count of numbers in [1,i] which are co-prime with i
+
+void precompute() {
+    for(int i=1; i<=N; i++) phi[i] = i;
+    
+    for(int i=2; i<=N; i++)
+        if(phi[i] == i) //i is a prime
+            for(int j=i; j<=N; j+=i) {
+                phi[j] /= i;
+                phi[j] *= (i-1);
+            }
+}
+
+int main() {
+    precompute();
+}
